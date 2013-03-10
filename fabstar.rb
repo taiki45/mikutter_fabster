3846e4f41667a103d69db3eaa7f1421419ad9bb6
Taiki ONO <taiks.4559@gmail.com>
2013-03-11 00:55:33 +0900

    init

---
+++ fabstar.rb
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+require 'mongo'
+require 'pry'
+
+module Fabstar
+  class Datastore
+    def initialize(id)
+      @id = id
+    end
+
+    def client
+      @client ||= Mongo::MongoClient.new
+    end
+
+    def tweets
+      client.db("tweeamon").cllection("tweets")
+    end
+
+    def my_mosts
+      tweets.find({"user.id" => @id })
+    end
+  end
+
+  Plugin.create :fabstar do
+    #store = Datastore.new(id)
+    binding.pry
+  end
+end
